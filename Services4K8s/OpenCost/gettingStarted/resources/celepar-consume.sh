91a1718daee0a01893624cafcc729600bd8e8458
tarsoqueiroz <tarsoqueiroz@gmail.com>
2024-07-05 10:59:06 -0300

    OpenCost: starting tests

---
+++ Services4K8s/OpenCost/gettingStarted/resources/celepar-consume.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -B
+
+# for i in $(seq 1 1000); do curl -s -o /dev/null "http://bookinfo.0a0f122c.nip.io:8080/productpage"; done
+
+for i in {1..1080000}; do
+
+  echo ${i}
+  curl -s -o /dev/null "http://celepar.0a0f122c.nip.io:8080/productpage"
+  sleep .16s
+
+done
